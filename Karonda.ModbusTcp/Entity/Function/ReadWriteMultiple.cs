dcb6d61fc4e2b0f9416a1a37349ec993ffe15f91
victorbu <victorbu.xx@gmail.com>
2019-01-31 15:21:17 +0800

    add whether read/write quantities is out of range check

--- Karonda.ModbusTcp/Entity/Function/ReadWriteMultiple.cs
+++ Karonda.ModbusTcp/Entity/Function/ReadWriteMultiple.cs
@@ -7,6 +7,12 @@ namespace Karonda.ModbusTcp.Entity.Function
 {
     public abstract class ReadWriteMultiple : ModbusFunction
     {
+        private readonly ushort MaxReadCoilsQuantity = 0x07D0;// 2000
+        private readonly ushort MaxWriteCoilsQuantity = 0x07B0;// 1968
+        private readonly ushort MaxReadRegistersQuantity = 0x007D;// 125
+        private readonly ushort MaxWriteRegistersQuantity = 0x007B;//123
+        private readonly string QuantityOutOfRange = "quantities must no more than {0}";
+
         public ushort StartingAddress { get; private set; }
         public ushort Quantity { get; private set; }
 
@@ -20,6 +26,8 @@ namespace Karonda.ModbusTcp.Entity.Function
         {
             StartingAddress = startingAddress;
             Quantity = quantity;
+
+            WhetherQuantityIsOutOfRange();
         }
 
         public override int CalculateLength()
@@ -30,6 +38,8 @@ namespace Karonda.ModbusTcp.Entity.Function
         {
             StartingAddress = buffer.ReadUnsignedShort();
             Quantity = buffer.ReadUnsignedShort();
+
+            WhetherQuantityIsOutOfRange();
         }
         public override IByteBuffer Encode()
         {
@@ -40,6 +50,35 @@ namespace Karonda.ModbusTcp.Entity.Function
             buffer.WriteUnsignedShort(Quantity);
 
             return buffer;
+        }
+
+        private void WhetherQuantityIsOutOfRange()
+        {
+            var exceptionMessage = string.Empty;
+            switch ((ModbusCommand)FunctionCode)
+            {
+                case ModbusCommand.ReadCoils:
+                case ModbusCommand.ReadDiscreteInputs:
+                    if (Quantity > MaxReadCoilsQuantity)
+                        exceptionMessage = string.Format(QuantityOutOfRange, MaxReadCoilsQuantity);
+                    break;
+                case ModbusCommand.ReadHoldingRegisters:
+                case ModbusCommand.ReadInputRegisters:
+                    if (Quantity > MaxReadRegistersQuantity)
+                        exceptionMessage = string.Format(QuantityOutOfRange, MaxReadRegistersQuantity);
+                    break;
+                case ModbusCommand.WriteMultipleCoils:
+                    if (Quantity > MaxWriteCoilsQuantity)
+                        exceptionMessage =  string.Format(QuantityOutOfRange, MaxWriteCoilsQuantity);
+                    break;
+                case ModbusCommand.WriteMultipleRegisters:
+                    if (Quantity > MaxWriteRegistersQuantity)
+                        exceptionMessage =  string.Format(QuantityOutOfRange, MaxWriteRegistersQuantity);
+                    break;
+            }
+
+            if (string.IsNullOrEmpty(exceptionMessage))
+                throw new ArgumentOutOfRangeException(exceptionMessage);
         }
     }
 }
